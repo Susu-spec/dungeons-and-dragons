ca2d92f19e5627cb30d4e6e697f983255251b2a0
Susu-spec <suleimansuwaibat@gmail.com>
2025-01-13 14:38:52 +0100

    chore: adjust view page for responsiveness

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -31,7 +31,7 @@ export default function ViewRow({ data, CardContent }: ViewRowProps) {
         {open && (
           <div className=" w-full h-full mx-auto max-h-[10.5rem] md:max-h-[20.5rem] md:max-w-[30rem]">
             <motion.div
-              className="fixed inset-x-auto inset-y-0 md:inset-x-[30%] md:inset-y-[20%] md:!pt-0 w-full h-full bg-transparent mx-auto max-h-[35rem] md:max-h-[40.5rem] md:max-w-[30rem] z-50"
+              className="fixed inset-x-0 inset-y-[20%] md:inset-x-[30%] md:inset-y-[20%] md:!pt-0 w-full h-full bg-transparent mx-auto max-h-[40.5rem] md:max-w-[30rem] z-50"
               initial={{ y: "100%", opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               exit={{ y: "100%", opacity: 0 }}
